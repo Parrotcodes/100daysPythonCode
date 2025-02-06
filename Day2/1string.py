99febf434b8383685995fdb98eb90be98000a366
rakesh <atrimyself@gmail.com>
2025-02-06 16:59:44 +0530

    Day2 added empty files only

---
+++ Day2/1string.py
@@ -0,0 +1,44 @@
+#Data Types
+#type()
+#lowercase
+#uppercase
+#titlecase
+#capitalize
+
+
+#String
+
+#subscript
+#sum of two numbers
+
+#integer
+
+#operations
+#PEMDAS
+# ()
+# **
+# * /
+# + -
+print(2+3*4)
+
+#number manipluation - +=, /=, *=, -= ,//=, %=, **=
+
+#fstrings - score = 0, name = "John" - f"{John}'s score is {0}"
+
+
+
+
+#float
+#complex
+#boolean
+#list
+#tuple
+#dictionary
+#set
+#frozenset
+#bytes
+#bytearray
+#range
+#None
+#Ellipsis
+#NotImplemented
