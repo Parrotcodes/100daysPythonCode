54b0f4f1270d6da507e9798922b0c5822923de67
rakesh <atrimyself@gmail.com>
2025-02-16 21:27:06 +0530

    day6 few files and all ppt days slides added

--- DayWiseCode/Day5/1loops.py
+++ DayWiseCode/Day5/1loops.py
@@ -5,3 +5,46 @@
 
 
 # lists using loops
+fruits = ["apple","banana","grapes"]
+
+for fruit in fruits:
+    print(fruit)
+    
+
+# weights = [180,124,165,173,189,169,146]
+std_weights = input("Input list of student weights: ").split()
+for n in range(0,len(std_weights)):
+    std_weights[n] = int(std_weights[n])
+    
+print(std_weights)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########## Solution #########
+# total_weight = 0
+
+# for weight in std_weights:
+#     total_weight += weight 
+# print(total_weight)
+
+# no_stds = 0
+# for std in std_weights:
+#     no_stds += 1
+# print(no_stds)
+
+# avg_weight = round(total_weight / no_stds)
+# print(avg_weight)
---
+++ DayWiseCode/Day6/1fun.py
@@ -0,0 +1,12 @@
+### Functions in Python ###
+
+# A function is a block of code which only runs when it is called.
+# You can pass data, known as parameters, into a function.
+# A function can return data as a result.
+
+# Syntax:
+def my_function():
+  print("Hello from a function")
+  
+# Calling function
+my_function()
