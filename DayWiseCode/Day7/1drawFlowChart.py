ff63c784803a330ae17ae0f8dc1186f0624ffd27
rakesh <atrimyself@gmail.com>
2025-02-21 21:11:42 +0530

    Day 7 starting files added

---
+++ DayWiseCode/Day7/1drawFlowChart.py
@@ -0,0 +1,17 @@
+# Drawing a flow chart
+# Flow chart Understanding Concepts
+
+# Steps:
+# 1. Generate a random Word
+# 2. Generate no.of blanks of that word
+# 3. Guess the a letter
+#   if yes == replace the blank with letter exact position
+        # all the blanks filled or not
+            # if No == repeat guessing a another letter
+            # else Game Over
+#   else 
+        # start hangman drawing
+        # lose a life
+        # check this Have the run our of lives
+            # if yes == repeat guess a another letter 
+            # else Game over
---
+++ DayWiseCode/Day7/Project Hangman.py
@@ -0,0 +1,30 @@
+# Hangman Project
+
+# Solution Steps:
+
+# Task 1
+# step1: create a list of items
+# step2: select random word every time from the list
+# step3: guess a letter from the list
+# step4: check the guess one of the letter in the choosen word
+
+# Task 2
+# step1: display the no.of blanks == random seleted word letters
+# step2: replace the letter at the correct position if the letter matched with the guessing letter
+# display the replaced letters and remainig as blanks
+
+# Task 3
+# check the display blanks are no longer avalilable then print "you win"
+
+
+# Task 4
+# import all the symbols of hangman as a list 
+# use random method and select the random list items from another list module
+
+
+# Task 5
+# Refer module section
+# make better User experience
+# Add logo
+# add random words more / generate more (using module concept)
+# remove the repetation of previous guessing letter / img
