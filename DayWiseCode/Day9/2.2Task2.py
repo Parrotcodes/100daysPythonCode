1016366a7120a3734272f200218f565973597433
rakesh <atrimyself@gmail.com>
2025-03-04 23:26:28 +0530

    day9 project done and day10 topic covered

--- DayWiseCode/Day9/2.2Task2.py
+++ DayWiseCode/Day9/2.2Task2.py
@@ -1,6 +1,6 @@
 # Task 2
 
-# first_list ==> list of countries
+# first_list ==> list of countries given
 #  new_dict = {}
 #  def add_new(par1,par2,par3)
 #      add new country 
@@ -9,3 +9,24 @@
 #  add_new("russia",34, ["rojak","kdfjke"])
 
 #  prev dictionay add the new dictionary
+
+
+
+
+
+
+
+
+
+
+
+
+# addCounty = input("Enter your country: ")
+# cities_vistited = input("Enter cities: ").split(",") # List of values
+# total_vistis = int(input("Enter how many times visited: "))
+
+# def add_new_country(country,cities_vis,total):
+#     #stms
+
+# add_new_country(addCounty,cities_vistited,total_vistis)
+# print(travel_log)
---
+++ DayWiseCode/Day9/BiddingProject.py
@@ -0,0 +1,32 @@
+print("Welcome to the Secret Acution Program")
+
+new_bidder = False
+bids ={}
+winner = ""
+
+# This function is used for checking the highest bid_amount among all the bidders
+def highest_Bidder(bidding_record):
+    highest_price = 0 #21 100
+    for bidder in bidding_record:
+        if bid_amount > highest_price:
+            highest_price  = bid_amount
+        winner = bidder
+    
+    print(f"The winner is {winner} with a bid of {highest_price}")
+
+# while loop continuously runnning based new_bidder condition
+while not new_bidder:
+    name = input("What is your name? ")
+    bid_amount = int(input("What is your bid amount? $"))
+
+    # {"rakesh": $21},{"raj" : $100}
+    bids[name] = bid_amount
+    
+    ycontinue = input("Are there any other bidders? Type 'yes' or 'no' ")
+    if ycontinue == "no":
+        new_bidder = True 
+        highest_Bidder(bids)  
+    elif ycontinue == "yes":
+        print("-------- clear screen -----")
+        
+
