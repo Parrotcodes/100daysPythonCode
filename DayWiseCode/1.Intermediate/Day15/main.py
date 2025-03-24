8c8b6898de80bebf02bc012107106d39ffe3fe4a
rakesh <atrimyself@gmail.com>
2025-03-24 11:46:34 +0530

    added intermediate folder to pycharm and started coffe machanine project

---
+++ DayWiseCode/1.Intermediate/Day15/main.py
@@ -0,0 +1,46 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+is_on = True
+while is_on:
+    user_choice = input("What would you like? (espresso/latte/cappuccino):")
+
+    if user_choice == "off":
+        is_on = False
+    elif user_choice == "report":
+        for item in resources:
+            print(f"{item} : {resources[item]}")
+        print(f"Money : {profit}")
+    else:
+        drink = MENU[user_choice]
+        print(drink)
